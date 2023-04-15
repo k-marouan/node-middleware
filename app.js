e19dfd6d6114553a935ed6fbca99893ef8fe0fa5
k-marouan <marouan.kachach@gmail.com>
2023-04-15 02:44:11 +0000

    node-middleware

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const morgan = require('morgan');
+
+// express app
+const app = express();
+
+// register view engine
+app.set('view engine', 'ejs'); // if your forder's name "view"
+
+// listen for requests
+app.listen(3000);
+
+// middleware & static files
+app.use(express.static('public')); // public = the folder name
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+    const blogs = [
+        { title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur' },
+        { title: 'Marouan finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur' },
+        { title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur' },
+    ];
+    res.render('index', { title: 'Home', blogs });
+});
+
+
+app.get('/about', (req, res) => {
+    res.render('about', { title: 'About' });
+});
+
+app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: 'Create a new Blog' })
+});
+
+// 404 page, the code below should be in the very bottom to work correctly
+app.use((req, res) => {
+    res.status(404).render('404', { title: '404' });
+});
